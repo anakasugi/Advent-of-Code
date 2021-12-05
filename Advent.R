8f8e935d1bea3f67d5424bbdfd7225a2d3b49239
anakasugi <89901268+anakasugi@users.noreply.github.com>
2021-12-06 04:04:45 +0900

    Update Advent.R

--- Advent.R
+++ Advent.R
@@ -10,7 +10,7 @@ day1 <- day1[1:2] %>%
   rename(state = X2)
 
 for (i in 1:length(day1)){
-  if (day1$Levels[i] <= day1$Levels[i+1]) {
+  if (day1$Levels[i] < day1$Levels[i+1]) {
     day1$state[i+1] <- "Increased"
   } else {
     day1$state[i+1] <- "Decreased"
@@ -23,4 +23,26 @@ state.tab # "Increased" 1564 times
 
 # Day 1: Problem 2 --------------------------------------------------------
 
+# Q: Same as above, but with rolling sums of 3-row windows
 
+# Re-run import for a clean slate
+day1 <- day1[1:3] %>% 
+  rename(total = X2) %>% 
+  rename(state = X3)
+
+for (i in 1:nrow(day1)){
+  day1$total[i] <- sum(day1$Levels[i:(i+2)])
+}
+
+for (i in 1:nrow(day1)){
+  if (day1$total[i] < day1$total[i+1]) {
+    day1$state[i+1] <- "Increased"
+  } else if (day1$total[i] > day1$total[i+1]){
+    day1$state[i+1] <- "Decreased"
+  } else {
+    day1$state[i+1] <- "No change"
+  }
+}
+
+state.tab <- table(day1$state)
+state.tab # "Increased" 1611 times
